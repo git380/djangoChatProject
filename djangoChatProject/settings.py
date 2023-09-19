140fdc3fd33ee01ec90b6cee8d80d5c485e31560
git380 <108381952+git380@users.noreply.github.com>
2023-09-19 22:32:33 +0900

    ログイン機能(MySQL)

--- djangoChatProject/settings.py
+++ djangoChatProject/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangoChatProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydatabase',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- webSocket/views.py
+++ webSocket/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from webSocket.models import LoginInfo
 
 
 def index(request):
     return render(request, 'webSocket/index.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        # login.htmlからPOSTされた内容を取得
+        username = request.POST['username']
+        password = request.POST['password']
+        print(f"Received username: {username}, password: {password}")
+        # データベースの内容をすべて出力
+        login_info_list = LoginInfo.objects.all()
+        for login_info in login_info_list:
+            print(f"Username: {login_info.username}, Password: {login_info.password}")
+        # nameとpwが一致した場合try 一致しなかった場合except
+        try:
+            login_info = LoginInfo.objects.get(username=username, password=password)
+            return render(request, 'loginok.html')
+        except LoginInfo.DoesNotExist:
+            return render(request, 'loginng.html')
+    return render(request, 'login.html')
