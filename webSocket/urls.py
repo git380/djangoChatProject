6002d889e0882838f1c5e9002551460d10c4569d
git380 <108381952+git380@users.noreply.github.com>
2023-10-30 12:17:14 +0900

    調整

--- webSocket/urls.py
+++ webSocket/urls.py
@@ -2,13 +2,16 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
+    path('login_ok/', views.login_ok, name='login_ok'),
     path('chat_list/', views.chat_list, name='chat_list'),
-    path('chat/<str:toid>/', views.chat, name='chat'),
+    path('chat/', views.chat, name='chat'),
     path('contact/', views.contact, name='contact'),
     path('logout/', views.logout, name='logout'),
     path('info_list/', views.info_list, name='info_list'),
-    path('info_update/<str:login_id>/', views.info_update, name='info_update'),
     path('login_info_add/', views.login_info_add, name='login_info_add'),
+    path('info_update/<str:login_id>/', views.info_update, name='info_update'),
+    path('info_update/', views.info_update, name='info_update_noid'),
     path('delete_info/<str:login_id>/', views.delete_info, name='delete_info'),
+    path('delete_info/', views.delete_info, name='delete_info_noid'),
     path('message_history/', views.message_history, name='message_history'),
 ]
--- webSocket/views.py
+++ webSocket/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, redirect
 from webSocket.models import LoginInfo
 from hashing import hashing
@@ -16,21 +16,30 @@ def login(request):
         # login.htmlからPOSTされた内容を取得
         login_id = request.POST['login_id']
         password = request.POST['password']
-        print(f"Received username: {login_id}, password: {password}")
-        # ハッシュ化
-        password = hashing(login_id, password)
         # nameとpwが一致した場合try 一致しなかった場合except
         try:
-            login_info = LoginInfo.objects.get(login_id=login_id, password=password)
-            request.session['login_id'] = login_id  # ユーザー名をセッションに保存
-            request.session['username'] = login_info.name  # "name"をセッションnameに俩存
-            request.session['role'] = login_info.role  # "role"をセッションに保存
-            if login_info.role == 3:
-                return render(request, 'administrator/loginOK.html')
-            return render(request, 'login/loginOK.html')
+            login_info = LoginInfo.objects.get(login_id=login_id, password=hashing(login_id, password))
+            request.session['login_id'] = login_id  # ユーザidをセッションに保存
+            request.session['username'] = login_info.name  # 名前をセッションusernameに保存
+            request.session['role'] = login_info.role  # ロールをセッションに保存
+            return redirect('login_ok')
         except LoginInfo.DoesNotExist:
             return render(request, 'login/loginNG.html')
     return render(request, 'login/login.html')
+
+
+def login_ok(request):
+    login_id = request.session.get('login_id')  # セッションからlogin_idを取得
+    # ログインしていない場合はログインページにリダイレクト
+    if not login_id:
+        return redirect('login')
+
+    # セッションのroleと比較
+    if request.session.get('role') == 3:
+        print(f'管理者 ログイン:{login_id}')
+        return render(request, 'administrator/loginOK.html')
+    print(f'ログイン:{login_id}')
+    return render(request, 'login/loginOK.html')
 
 
 def chat_list(request):
@@ -39,33 +48,37 @@ def chat_list(request):
     if not login_id:
         return redirect('login')
 
-    partner_list = LoginInfo.objects.exclude(role=3).exclude(login_id=login_id).all()
-    return render(request, 'chat/chat_list.html', {'partner_list': partner_list})
+    return render(request, 'chat/chat_list.html',
+                  {'partner_list': LoginInfo.objects.exclude(role=3).exclude(login_id=login_id).all()})
 
 
-def chat(request, toid):
+def chat(request):
     login_id = request.session.get('login_id')
     # ログインしていない場合はログインページにリダイレクト
     if not login_id:
         return redirect('login')
 
-    to_info = LoginInfo.objects.get(login_id=toid)
-    chat_info = {'toNameInput': to_info.name, 'toInput': toid, 'idInput': login_id}
-    return render(request, 'chat/chat.html', {'chat_info': chat_info})
+    if request.method == 'POST':
+        toid = request.POST['toid']
+        if not toid:
+            return redirect('chat_list')
+
+        to_info = LoginInfo.objects.filter(login_id=toid).values('name').first()
+        chat_info = {'toNameInput': to_info['name'], 'toInput': toid, 'idInput': login_id}
+        return render(request, 'chat/chat.html', {'chat_info': chat_info})
+    return redirect('chat_list')
 
 
 def contact(request):
     username = request.session.get('username')
     role = request.session.get('role')
-    # ログインしていない場合はログインページにリダイレクト
-    if not username:
-        return redirect('login')
 
     if role == 1:
         return render(request, 'contact/student.html', {'name': username})
     elif role == 2:
         return render(request, 'contact/teacher.html', {'name': username})
     else:
+        # ログインしていない・ロールが一致しない場合はログインページにリダイレクト
         return redirect('login')
 
 
@@ -79,14 +92,41 @@ def info_list(request):
     if request.session.get('role') != 3:
         return redirect('login')
 
-    login_info_list = LoginInfo.objects.all()
-    return render(request, 'administrator/login_info_list.html', {'login_info_list': login_info_list})
+    return render(request, 'administrator/login_info_list.html', {'login_info_list': LoginInfo.objects.all()})
 
 
-def info_update(request, login_id):
+def login_info_add(request):
     # ログインしていない場合もしくは管理者でない場合
     if request.session.get('role') != 3:
         return redirect('login')
+
+    if request.method == 'POST':
+        login_id = request.POST['login_id']
+        # データベースにIDがある場合
+        if LoginInfo.objects.filter(login_id=login_id).exists():
+            return HttpResponse('既に登録されているIDです。')
+
+        # DBへ追加
+        LoginInfo(
+            login_id=login_id,
+            name=request.POST['name'],
+            password=hashing(login_id, request.POST['password']),
+            address=request.POST['address'],
+            role=request.POST['role']
+        ).save()
+
+        return redirect('info_list')
+
+    return render(request, 'administrator/login_info_add.html')
+
+
+def info_update(request, login_id=None):
+    # ログインしていない場合もしくは管理者でない場合
+    if request.session.get('role') != 3:
+        return redirect('login')
+    # login_id パラメータがない場合404を返す
+    if login_id is None:
+        return HttpResponseNotFound("login_idがありません")
 
     login_info = LoginInfo.objects.get(login_id=login_id)
 
@@ -102,42 +142,18 @@ def info_update(request, login_id):
             login_info.role = request.POST['role']
         login_info.save()
 
-        print(f"Received: {login_info}")
         return redirect('info_list')
 
     return render(request, 'administrator/login_info_update.html', {'login_info': login_info})
 
 
-def login_info_add(request):
+def delete_info(request, login_id=None):
     # ログインしていない場合もしくは管理者でない場合
     if request.session.get('role') != 3:
         return redirect('login')
-
-    if request.method == 'POST':
-        login_id = request.POST['login_id']
-        # データベースにIDがある場合
-        if LoginInfo.objects.filter(login_id=login_id).exists():
-            return HttpResponse('既に登録されているIDです。')
-
-        # DBへ追加
-        login_info = LoginInfo(
-            login_id=login_id,
-            name=request.POST['name'],
-            password=hashing(login_id, request.POST['password']),
-            address=request.POST['address'],
-            role=request.POST['role']
-        )
-        login_info.save()
-
-        return redirect('info_list')
-
-    return render(request, 'administrator/login_info_add.html')
-
-
-def delete_info(request, login_id):
-    # ログインしていない場合もしくは管理者でない場合
-    if request.session.get('role') != 3:
-        return redirect('login')
+    # login_id パラメータがない場合404を返す
+    if login_id is None:
+        return HttpResponseNotFound("login_idがありません")
 
     login_info = LoginInfo.objects.get(login_id=login_id)
 
@@ -153,15 +169,12 @@ def message_history(request):
     if request.session.get('role') != 3:
         return redirect('login')
 
-    id_list = LoginInfo.objects.values_list('login_id', flat=True)
-
     if request.method == 'POST':
         message_list = []
         id1 = request.POST['name1']
         id2 = request.POST['name2']
-        url = 'https://erygod339e.execute-api.us-east-1.amazonaws.com/rest'
-        chat_history = requests.post(url, data='chat_history').json()
-        for message_id, message in chat_history.items():  # 辞書の中身(JSON)をすべて送信する
+        for message_id, message in requests.post('https://erygod339e.execute-api.us-east-1.amazonaws.com/rest',
+                                                 data='chat_history').json().items():  # 辞書の中身(JSON)をすべて取り出す
             data = json.loads(message)
             if data['client_id'] == id1 and data['to_client'] == id2:
                 # id1 から id2 に送信された
@@ -171,4 +184,5 @@ def message_history(request):
                 message_list.append(f"{id2}:{data['message']}")
         return render(request, 'administrator/chat_history.html', {'message_list': message_list})
 
-    return render(request, 'administrator/all_chat_history.html', {'id_list': id_list})
+    return render(request, 'administrator/all_chat_history.html',
+                  {'id_list': LoginInfo.objects.values_list('login_id', flat=True)})
